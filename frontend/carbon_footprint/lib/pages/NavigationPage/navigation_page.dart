2bf31c24169e13419d2001a4fede71e5517beaf0
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-09-23 19:13:41 -0500

    Mapbox route building poc

--- frontend/carbon_footprint/lib/pages/NavigationPage/navigation_page.dart
+++ frontend/carbon_footprint/lib/pages/NavigationPage/navigation_page.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_field, prefer_final_fields
 
-import 'package:carbon_footprint/NavigationSidebar.dart';
-import 'package:carbon_footprint/constants.dart';
 import 'package:carbon_footprint/pages/ChatPage/mobileChatPage.dart';
-import 'package:carbon_footprint/pages/ExampleStrucutre/widgets/useful_box.dart';
 import 'package:carbon_footprint/pages/ExampleStrucutre/widgets/useful_tile.dart';
 import 'package:carbon_footprint/pages/Home/home_page_desktop.dart';
 import 'package:carbon_footprint/pages/Home/home_page_mobile.dart';
@@ -11,6 +8,8 @@ import 'package:carbon_footprint/pages/Home/home_page_tablet.dart';
 import 'package:carbon_footprint/providers/pageControl.dart';
 import 'package:carbon_footprint/responsive/responsive_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MobileCarbonReduction extends StatefulWidget {
@@ -24,6 +23,66 @@ class _MobileCarbonReductionState extends State<MobileCarbonReduction> {
   final pageControlProvider = ChangeNotifierProvider<PageControlNotifier>(
     (ref) => PageControlNotifier(),
   );
+  String? _instruction;
+  String? _platformVersion;
+  bool _isMultipleStop = false;
+  double? _distanceRemaining, _durationRemaining;
+  MapBoxNavigationViewController? _controller;
+  bool _routeBuilt = false;
+  bool _isNavigating = false;
+  bool _inFreeDrive = false;
+  late MapBoxOptions _navigationOption;
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initialize() async {
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    _navigationOption = MapBoxNavigation.instance.getDefaultOptions();
+    _navigationOption.simulateRoute = true;
+    //_navigationOption.initialLatitude = 36.1175275;
+    //_navigationOption.initialLongitude = -115.1839524;
+    MapBoxNavigation.instance.registerRouteEventListener(_onEmbeddedRouteEvent);
+    MapBoxNavigation.instance.setDefaultOptions(_navigationOption);
+
+    String? platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await MapBoxNavigation.instance.getPlatformVersion();
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  final _home = WayPoint(
+      name: "Home",
+      latitude: 37.77440680146262,
+      longitude: -122.43539772352648,
+      isSilent: false);
+
+  final _store = WayPoint(
+      name: "Store",
+      latitude: 37.76556957793795,
+      longitude: -122.42409811526268,
+      isSilent: false);
 
   @override
   Widget build(BuildContext context) {
@@ -94,30 +153,131 @@ class _MobileCarbonReductionState extends State<MobileCarbonReduction> {
           ),
           backgroundColor: Colors.grey[300],
           body: Column(children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: SizedBox(
-                width: double.infinity,
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    return UsefulBox();
-                  },
+            SizedBox(
+              height: 100,
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: SizedBox(
+                  height: 400,
+                  child: MapBoxNavigationView(
+                    options: _navigationOption,
+                    onRouteEvent: _onEmbeddedRouteEvent,
+                    onCreated:
+                        (MapBoxNavigationViewController controller) async {
+                      _controller = controller;
+                      controller.initialize();
+                    },
+                  ),
                 ),
               ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _isNavigating
+                      ? null
+                      : () {
+                          if (_routeBuilt) {
+                            _controller?.clearRoute();
+                          } else {
+                            var wayPoints = <WayPoint>[];
+                            wayPoints.add(_home);
+                            wayPoints.add(_store);
+                            _isMultipleStop = wayPoints.length > 2;
+                            _controller?.buildRoute(
+                                wayPoints: wayPoints,
+                                options: _navigationOption);
+                          }
+                        },
+                  child: Text(_routeBuilt && !_isNavigating
+                      ? "Clear Route"
+                      : "Build Route"),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: _routeBuilt && !_isNavigating
+                      ? () {
+                          _controller?.startNavigation();
+                        }
+                      : null,
+                  child: const Text('Start '),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: _isNavigating
+                      ? () {
+                          _controller?.finishNavigation();
+                        }
+                      : null,
+                  child: const Text('Cancel '),
+                )
+              ],
+            ),
             Expanded(
-                child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return UsefuleTile();
-              },
-            ))
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return UsefuleTile();
+                },
+              ),
+            )
           ]),
         );
       },
     );
+  }
+
+  Future<void> _onEmbeddedRouteEvent(e) async {
+    _distanceRemaining = await MapBoxNavigation.instance.getDistanceRemaining();
+    _durationRemaining = await MapBoxNavigation.instance.getDurationRemaining();
+
+    switch (e.eventType) {
+      case MapBoxEvent.progress_change:
+        var progressEvent = e.data as RouteProgressEvent;
+        if (progressEvent.currentStepInstruction != null) {
+          _instruction = progressEvent.currentStepInstruction;
+        }
+        break;
+      case MapBoxEvent.route_building:
+      case MapBoxEvent.route_built:
+        setState(() {
+          _routeBuilt = true;
+        });
+        break;
+      case MapBoxEvent.route_build_failed:
+        setState(() {
+          _routeBuilt = false;
+        });
+        break;
+      case MapBoxEvent.navigation_running:
+        setState(() {
+          _isNavigating = true;
+        });
+        break;
+      case MapBoxEvent.on_arrival:
+        if (!_isMultipleStop) {
+          await Future.delayed(const Duration(seconds: 3));
+          await _controller?.finishNavigation();
+        } else {}
+        break;
+      case MapBoxEvent.navigation_finished:
+      case MapBoxEvent.navigation_cancelled:
+        setState(() {
+          _routeBuilt = false;
+          _isNavigating = false;
+        });
+        break;
+      default:
+        break;
+    }
+    setState(() {});
   }
 }
